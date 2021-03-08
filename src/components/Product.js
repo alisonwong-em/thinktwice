04130ea42d56ecd9cfbcdde71e34eafe45b6c2ef
michellebraun <michellebraun1342@gmail.com>
2021-03-07 22:16:20 -0500

    Tabs and other adjustments

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,82 +10,97 @@ class Product extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      categoryText: '',
+      categoryText: 'Description of Product',
+      hovering: false,
       pass: false,
     }
 
-    this.recycleClick = this.recycleClick.bind(this);
-    this.organicEffClick = this.organicClick.bind(this);
-    this.plasticClick = this.plasticClick.bind(this);
+    this.recycleHover = this.recycleHover.bind(this);
+    this.organicHover = this.organicHover.bind(this);
+    this.plasticHover = this.plasticHover.bind(this);
+    this.categoryLeave = this.categoryLeave.bind(this);
   }
 
-  recycleClick() {
-    this.setState({ pass: true, categoryText: 'H&M scores a A- in this category' });
+  recycleHover() {
+    this.setState({ hovering: true, pass: true, categoryText: 'H&M scores a A- in this category' });
   }
 
-  organicClick() {
-    this.setState({ pass: true, categoryText: 'H&M is part of various' });
+  organicHover() {
+    this.setState({ hovering: true, pass: true, categoryText: 'H&M is part of various' });
   }
 
-  plasticClick() {
-    this.setState({ pass: true, categoryText: 'H&M scores a A+ in this category' });
+  plasticHover() {
+    this.setState({ hovering: true, pass: true, categoryText: 'H&M scores a A+ in this category' });
+  }
+
+  categoryLeave() {
+    this.setState({ hovering: false, categoryText: 'Description of Product' });
   }
 
   render() {
   // These would change based on info fetched from DB
   let productName = 'Turtleneck Sweater';
-  let sustainability = 'Unsustainable';
-  let { pass, categoryText } = this.state;
+  let sustainable = false;
+  let { hovering, pass, categoryText } = this.state;
 
   return (
-    <div>
+    <div className='mainContent'>
       <h2 className='heading'>{productName}</h2>
-      <div class='sustainable'>{sustainability}</div>
-      <div class='row' align='center'>
-        <div class='categoryColumn'>
-          <button className='recycleMat' onMouseOver={this.recycleClick}>
-            Recycled Material
-          </button>
-          <button className='orgMat' onMouseEnter={this.organicClick}>
-            Organic Material
-          </button>
-          <button className='plasticFree' onMouseEnter={this.plasticClick}>
-            Plastic Free
-          </button>
+      {sustainable ? 
+      <div class='sustainable'>Sustainable</div>
+      : <div class='unsustainable'>Unsustainable</div>
+      }
+      <div>
+        <div class='row' align='center'>
+          <div class='categoryColumn'>
+            <button className='recycleMat' onMouseOver={this.recycleHover} onMouseLeave={this.categoryLeave}>
+              Recycled Material
+            </button>
+            <button className='orgMat' onMouseEnter={this.organicHover} onMouseLeave={this.categoryLeave}>
+              Organic Material
+            </button>
+            <button className='plasticFree' onMouseEnter={this.plasticHover} onMouseLeave={this.categoryLeave}>
+              Plastic Free
+            </button>
+          </div>
+          <div class='categoryColumn'>
+            {hovering ? 
+              <p className='text'>{pass ? <b className='pass'>Pass <img src={ThumbsUp}/>: </b> : <b className='fail'>Fail <img src={ThumbsDown}/>: </b>}{categoryText}</p>
+              :
+              <p className='text'>{categoryText}</p>
+            }
+          </div>
         </div>
-        <div class='categoryColumn'>
-          <p className='text'>{pass ? <b className='pass'>PASS <img src={ThumbsUp}/>: </b> : <b className='fail'>FAIL<img src={ThumbsDown}/>: </b>}{categoryText}</p>
-        </div>
-      </div>
 
-      <h3><em>Think Twice about the necessity of this purchase</em></h3>
-      {/* Sustainable Alternatives */}
-      <h2 className='susAltHeading'>Sustainable Alternatives <img src={CircleCheck}/></h2>
-      <div class='row' align='center'>
-        {/* <div class='arrowColumn'>
-          <input type="image" src={LeftArrow} />
-        </div> */}
-        <div className='altsColumn'>
-          {/* <img /> */}
-          <p className='text'>Patagonia Sweater</p>
-          <p className='price'>$315.00</p>
+        <h3 className='thinkTwiceTxt'><em>Think Twice about the necessity of this purchase</em></h3>
+        {/* Sustainable Alternatives */}
+        <h2 className='susAltHeading'>Sustainable Alternatives <img src={CircleCheck}/></h2>
+        <div class='row' align='center'>
+          {/* <div class='arrowColumn'>
+            <input type="image" src={LeftArrow} />
+          </div> */}
+          <div className='leftAltCol'>
+            {/* <img /> */}
+            <p className='text'>Patagonia Sweater</p>
+            <p className='price'>$315.00</p>
+          </div>
+          <div className='rightAltCol'>
+            {/* <img /> */}
+            <p className='text'>Adidas Sweater</p>
+            <p className='price'>$75.99</p>
+          </div>
+          {/* <div class='arrowColumn'>
+            <input type="image" src={RightArrow} />
+          </div> */}
         </div>
-        <div className='altsColumn'>
-          {/* <img /> */}
-          <p className='text'>Adidas Sweater</p>
-          <p className='price'>$75.99</p>
-        </div>
-        {/* <div class='arrowColumn'>
-          <input type="image" src={RightArrow} />
-        </div> */}
-      </div>
 
-      {/* STUFF GOES HERE */}
-      <button className='learnBtn'>
-        <a href="www.thinktwice.eco" target="_blank">
-          Learn More
-        </a>
-      </button>
+        {/* STUFF GOES HERE */}
+        <button className='learnBtn'>
+          <a href="www.thinktwice.eco" target="_blank">
+            Learn More
+          </a>
+        </button>
+      </div>
     </div>
   )}
 }
